530f58c7759a5d343c328f00ad7452ce981f4ada
Pablo Parra <pablo.parra.1986@hotmail.com>
2018-03-19 17:46:14 -0300

    funcina botn guardar

--- Ingreso_Cliente_2/Form1.Designer.cs
+++ Ingreso_Cliente_2/Form1.Designer.cs
@@ -29,11 +29,273 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnActualiizar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            this.comboDate = new System.Windows.Forms.DateTimePicker();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtRut = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tabla = new System.Windows.Forms.DataGridView();
+            this.comboEstado = new System.Windows.Forms.ComboBox();
+            this.comboSexo = new System.Windows.Forms.ComboBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.tabla)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(517, 104);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 23);
+            this.btnModificar.TabIndex = 39;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(517, 142);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 38;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnActualiizar
+            // 
+            this.btnActualiizar.Location = new System.Drawing.Point(517, 181);
+            this.btnActualiizar.Name = "btnActualiizar";
+            this.btnActualiizar.Size = new System.Drawing.Size(75, 23);
+            this.btnActualiizar.TabIndex = 37;
+            this.btnActualiizar.Text = "Actualzar";
+            this.btnActualiizar.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(307, 429);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 36;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(470, 429);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 35;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(517, 66);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 34;
+            this.btnBuscar.Text = "Buscar Rut";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(164, 429);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 33;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(102, 245);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(65, 13);
+            this.label12.TabIndex = 32;
+            this.label12.Text = "Estado Civil:";
+            // 
+            // comboDate
+            // 
+            this.comboDate.Location = new System.Drawing.Point(247, 170);
+            this.comboDate.Name = "comboDate";
+            this.comboDate.Size = new System.Drawing.Size(200, 20);
+            this.comboDate.TabIndex = 31;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(102, 142);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(52, 13);
+            this.label11.TabIndex = 30;
+            this.label11.Text = "Apellidos:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(247, 104);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(200, 20);
+            this.txtNombre.TabIndex = 29;
+            this.txtNombre.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(247, 139);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(200, 20);
+            this.txtApellido.TabIndex = 28;
+            // 
+            // txtRut
+            // 
+            this.txtRut.Location = new System.Drawing.Point(247, 69);
+            this.txtRut.Name = "txtRut";
+            this.txtRut.Size = new System.Drawing.Size(200, 20);
+            this.txtRut.TabIndex = 27;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(102, 111);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(47, 13);
+            this.label10.TabIndex = 26;
+            this.label10.Text = "Nombre:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(102, 177);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(96, 13);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "Fecha Nacimiento:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(102, 212);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(34, 13);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "Sexo:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(102, 76);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(27, 13);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "Rut:";
+            // 
+            // tabla
+            // 
+            this.tabla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tabla.Location = new System.Drawing.Point(91, 276);
+            this.tabla.Name = "tabla";
+            this.tabla.Size = new System.Drawing.Size(501, 147);
+            this.tabla.TabIndex = 22;
+            // 
+            // comboEstado
+            // 
+            this.comboEstado.FormattingEnabled = true;
+            this.comboEstado.Items.AddRange(new object[] {
+            "debe elegir estado",
+            "soltero",
+            "casado",
+            "divorciado",
+            "viudo"});
+            this.comboEstado.Location = new System.Drawing.Point(247, 237);
+            this.comboEstado.Name = "comboEstado";
+            this.comboEstado.Size = new System.Drawing.Size(200, 21);
+            this.comboEstado.TabIndex = 21;
+            // 
+            // comboSexo
+            // 
+            this.comboSexo.FormattingEnabled = true;
+            this.comboSexo.Items.AddRange(new object[] {
+            "debe elegir sexo",
+            "hobre",
+            "mujer"});
+            this.comboSexo.Location = new System.Drawing.Point(247, 204);
+            this.comboSexo.Name = "comboSexo";
+            this.comboSexo.Size = new System.Drawing.Size(200, 21);
+            this.comboSexo.TabIndex = 20;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 485);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnActualiizar);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.comboDate);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtRut);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.tabla);
+            this.Controls.Add(this.comboEstado);
+            this.Controls.Add(this.comboSexo);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.tabla)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnActualiizar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.DateTimePicker comboDate;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtRut;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DataGridView tabla;
+        private System.Windows.Forms.ComboBox comboEstado;
+        private System.Windows.Forms.ComboBox comboSexo;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
     }
 }
 
--- Ingreso_Cliente_2/Form1.cs
+++ Ingreso_Cliente_2/Form1.cs
@@ -12,9 +12,72 @@ namespace Ingreso_Cliente_2
 {
     public partial class Form1 : Form
     {
+        private List<Cliente> lc = new List<Cliente>();
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (txtRut.Text == "")
+            {
+                errorProvider1.SetError(txtRut, "Debe Ingresar Rut");
+                txtRut.Focus();
+                return;
+
+            }
+            if (txtNombre.Text == "")
+            {
+                errorProvider1.SetError(txtNombre, "Debe Ingresar Nombre");
+                txtNombre.Focus();
+                return;
+
+            }
+            if (txtApellido.Text == "")
+            {
+                errorProvider1.SetError(txtApellido, "Debe Ingresar Apellido");
+                txtApellido.Focus();
+                return;
+
+            }
+            if (comboSexo.Text == "")
+            {
+                errorProvider1.SetError(comboSexo, "Debe Ingresar sexo");
+                comboSexo.Focus();
+                return;
+
+            }
+            if (comboEstado.Text == "")
+            {
+                errorProvider1.SetError(comboEstado, "Debe Ingresar Estado Civil");
+                comboEstado.Focus();
+                return;
+            }
+            Cliente cliente = new Cliente();
+            cliente.Rut = txtRut.Text;
+            cliente.Nombre = txtNombre.Text;
+            cliente.Apellido = txtApellido.Text;
+            cliente.F_nacimiento = comboDate.Value;
+            cliente.S = (Cliente.Sexo)this.comboSexo.SelectedIndex;
+            cliente.E = (Cliente.EstadoCivil)this.comboEstado.SelectedIndex;
+            lc.Add(cliente);
+            tabla.DataSource = null;
+            tabla.DataSource = lc;
+
+
+
+        }
+
+
+
+
     }
 }
+
